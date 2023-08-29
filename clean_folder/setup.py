7af446f8cdd98f734423cbb538818bde9ab8e8ac
SashaTheOk <o.kovalenko.koo@gmail.com>
2023-08-29 22:32:12 +0200

    1

---
+++ clean_folder/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='clean_folder',
+      version='1.00',
+      description='Clean Folder',
+      author='Kovalenko O.O.',
+      author_email='nomail@nomail.com',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['clean_folder=clean_folder.main:start']}
+)
